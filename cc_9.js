efe1f7cccc1e9577a71fc7a2f0d4bfb49fc60935
afridi00000 <afridi@usf.edu>
2025-02-19 18:40:07 -0500

    Task 2 - Created Manager Class with Inheritance

--- cc_9.js
+++ cc_9.js
@@ -19,3 +19,22 @@ class Employee {
 const emp1 = new Employee("Alice Johnson", 101, "Sales", 5000);
 console.log(emp1.getDetails()); 
 console.log(emp1.calculateAnnualSalary()); 
+// Task 2: Creating a Manager Class (Inheritance & Method Overriding)
+class Manager extends Employee {
+    constructor(name, id, department, salary, teamSize) {
+        super(name, id, department, salary);
+        this.teamSize = teamSize;
+    }
+
+    getDetails() {
+        return `Manager: ${this.name}, ID: ${this.id}, Department: ${this.department}, Salary: $${this.salary}, Team Size: ${this.teamSize}`;
+    }
+
+    calculateBonus() {
+        return this.calculateAnnualSalary() * 0.1;
+    }
+}
+
+const mgr1 = new Manager("John Smith", 201, "IT", 8000, 5);
+console.log(mgr1.getDetails()); 
+console.log(mgr1.calculateBonus()); 
